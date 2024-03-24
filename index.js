d069da31c7abf27f928fb7b549739d191376851d
ParkSeongGyeol <skp0626@naver.com>
2024-03-25 05:47:40 +0900

    test_last

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Hello!\n');
+  res.end('Hello, Hello! Hi! \n');
 });
 
 server.listen(port, hostname, () => {
